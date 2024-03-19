9545d29117358eecce11c0d5ea935eeb14fae76d
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-03-20 02:16:58 +0700

    add: Lec16_ArrayList

---
+++ 02-List/Lec16_List.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList<String> customers = new ArrayList<String>();
+        System.out.println("Hello World");
+        customers.add("Sommai");
+        customers.add("Somchai");
+        System.out.println(customers.get(0));
+    }
+}
